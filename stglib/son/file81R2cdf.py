f2af61560a62994a1fa33d1f39655c713bc9fc81
De Meo <odemeo@usg.gov>
2024-10-24 10:18:38 -0400

    added sonar raw2cdf

---
+++ stglib/son/file81R2cdf.py
@@ -0,0 +1,158 @@
+import os
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from . import sonutils
+from .core import utils
+
+
+def read_81R(fname):
+    # Parse an 81R file returned from an 881A-GS model imagenex sonar
+    # Input:
+    #    fname: The name of the .81R file being processed
+    # Outputs:
+    #    FanData: A dictionary containing the entire file as a numpy array of bytes
+    #        as well as the portion of the file that contains the sonar return data
+    #    FanHeader: A dictionary containing relevant information for processing the
+    #        sonar data that has been extracted from the various headers
+    #    FanSwitches: A dictionary containg the information extracted from the
+    #        universal file header
+
+    # Open and read the file in a binary format
+    fid = open(fname, "rb")
+    data = fid.read()
+
+    # Universal lengths of the file header and the device list
+    N81RFILEHEADERBYTES = 1024
+    NDEVICELISTBYTES = 1024
+
+    # Parse the start of the file
+    header = sonutils.parse_pingHeader(data[:N81RFILEHEADERBYTES], fname[-12:-8])[1]
+
+    # Gather the size of the switch commands and the return data header
+    switchCommandBytes = header["SwitchCommandBytes"]
+    returnHeaderBytes = header["ReturnHeaderBytes"]
+
+    # Drop unneeded variables
+    del header["SwitchCommandBytes"]
+    del header["ReturnHeaderBytes"]
+
+    # Extract information from the first set of switch commands
+    offset = N81RFILEHEADERBYTES + NDEVICELISTBYTES
+    SwitchSettings = sonutils.parse_switchCommand(
+        data[offset : offset + switchCommandBytes]
+    )
+
+    # Add switch settings and filename to header info
+    header.update(SwitchSettings)
+    header["FileName"] = fname
+
+    # #Extract information from the first return data header
+    # offset += switchCommandBytes
+    # ReturnHeader = sonutils.parseFanHeader(data[offset:offset+returnHeaderBytes], header)
+
+    # Calculate the number of pings using the file size and the size of each ping
+    npings = int(os.path.getsize(fname) / header["TotalBytes"])
+
+    # Convert the binary file into a 2-d numpy array
+    imagedata = np.array(bytearray(data)).reshape(npings, header["TotalBytes"])
+
+    # Initialize the dictionary
+    variables = {}
+
+    # Initialize variables to make the loop more efficient
+    variables["ReturnDataHeaderType"] = [""] * npings
+    variables["HeadID"] = [""] * npings
+    variables["HeadPosition"] = [0] * npings
+    variables["HeadAngle"] = [0] * npings
+    variables["StepDirection"] = [0] * npings
+    variables["Range"] = [0] * npings
+    variables["ProfileRange"] = [0] * npings
+    variables["NDataBytes"] = [0] * npings
+    variables["Heading"] = [0] * npings
+    variables["Pitch"] = [0] * npings
+    variables["Roll"] = [0] * npings
+    variables["time"] = [0] * npings
+    variables["StartGain"] = [0] * npings
+    variables["NReturnBytes"] = [0] * npings
+
+    # Extract data from each ping
+    for i in range(npings):
+        # Gather data from the universal header
+        time, PingHeader = sonutils.parse_pingHeader(
+            imagedata[i, :N81RFILEHEADERBYTES].tobytes(), fname[-12:-8]
+        )
+
+        # Gather data from the switch commands
+        offset = N81RFILEHEADERBYTES + NDEVICELISTBYTES
+        SwitchCommand = sonutils.parse_switchCommand(
+            imagedata[i, offset : offset + switchCommandBytes].tobytes()
+        )
+
+        # Gather data from the return data header
+        offset += switchCommandBytes
+        ReturnHeader = sonutils.parse_returnHeader(
+            imagedata[i, offset : offset + returnHeaderBytes].tobytes(), SwitchCommand
+        )
+
+        # If the header isn't empty, transfer the gathered data into the output dictionary
+        if ReturnHeader:
+            variables["ReturnDataHeaderType"][i] = ReturnHeader["ReturnDataHeaderType"]
+            variables["HeadID"][i] = ReturnHeader["HeadID"]
+            variables["HeadPosition"][i] = ReturnHeader["HeadPosition"]
+            variables["HeadAngle"][i] = ReturnHeader["HeadAngle"]
+            variables["StepDirection"][i] = ReturnHeader["StepDirection"]
+            variables["Range"][i] = ReturnHeader["Range"]
+            variables["ProfileRange"][i] = ReturnHeader["ProfileRange"]
+            variables["NDataBytes"][i] = ReturnHeader["NDataBytes"]
+            variables["Heading"][i] = ReturnHeader["Heading"]
+            variables["Pitch"][i] = ReturnHeader["Pitch"]
+            variables["Roll"][i] = ReturnHeader["Roll"]
+            variables["time"][i] = time
+            variables["StartGain"][i] = SwitchCommand["StartGain"]
+            if "NReturnBytes" in ReturnHeader:
+                variables["NReturnBytes"][i] = ReturnHeader["NReturnBytes"]
+            else:
+                variables["NReturnBytes"][i] = float("NaN")
+                print(f"Problem at ping {i}")
+
+    # Isolate the section of the numpy array that corresponds to the return data
+    offset = (
+        N81RFILEHEADERBYTES + NDEVICELISTBYTES + switchCommandBytes + returnHeaderBytes
+    )
+    variables["ImageData"] = imagedata[:, offset:-1]
+    variables["Raw"] = imagedata
+
+    # Convert to xarray
+    del variables["HeadID"]
+    del variables["ReturnDataHeaderType"]
+    del variables["NDataBytes"]
+    del variables["NReturnBytes"]
+
+    df = pd.DataFrame.from_dict(variables)
+    df.index.names = ["time"]
+    ds = xr.Dataset.from_dataframe(df)
+
+    return ds, variables, header
+
+
+# Make raw CDF
+def file81R_to_cdf(metadata):
+    basefile = metadata["basefile"]
+
+    FanData, FanHeader, FanSwitches = read_81R(basefile + ".81R")
+
+    ds = utils.write_metadata(ds, metadata)
+
+    ds = utils.ensure_cf(ds)
+
+    # configure file
+    cdf_filename = ds.attrs["filename"] + "-raw.cdf"
+
+    ds.to_netcdf(cdf_filename, unlimited_dims=["time"])
+
+    print("Finished writing data to %s" % cdf_filename)
+
+    return ds
---
+++ stglib/son/sonutils.py
@@ -0,0 +1,197 @@
+import datetime
+import struct
+from math import floor
+
+
+def bin2ascii(binString):
+    # Read a set of binary ASCII values and return a string
+    # Input:
+    #    binString: Binary ASCII value(s)
+    # Output:
+    #    output: String of characters from binString
+
+    # Create an array of characters
+    charArray = [chr(x) for x in binString]
+
+    # Convert the array of characters into one string
+    output = ""
+    for char in charArray:
+        output += char
+
+    # Remove ASCII null values (00 in hex) and return the string
+    return output.replace("\x00", "")
+
+
+def parse_pingHeader(fheader, timeprefix):
+    # Read the universal 81R header using the guide provided by the manual
+    # Inputs:
+    #    fheader: The file header in binary form
+    #    timeprefix: a four digit date string in the format of mmdd extracted
+    #        from the filename
+    # Outputs:
+    #    time: The time at the current ping
+    #    pingheader: A dictionary containing information extracted from the header
+
+    # Initialize the dictionary
+    pingheader = {}
+
+    # First 3 bytes read 81R
+    pingheader["ReturnDataHeaderType"] = bin2ascii(fheader[0:3])
+
+    # Get file size information based on the model of the sonar
+    temp = [128, 40]
+    pingheader["SwitchCommandBytes"] = temp[fheader[3] % 2]
+    temp = [256, 32]
+    pingheader["ReturnHeaderBytes"] = temp[fheader[3] % 2]
+
+    # Get file size
+    pingheader["TotalBytes"] = struct.unpack("<I", fheader[4:8])[0]
+
+    # Extract additional information
+    pingheader["Orientation"] = bin(fheader[319])[2:].zfill(8)[7]  # 1 is up
+    temp = ["Sector", "Polar", "Sidescan"]
+    pingheader["Mode"] = temp[fheader[324]]
+    pingheader["RangeOffset"] = floor(struct.unpack("<f", fheader[325:329])[0])
+
+    # pingheader['StepSize'] = floor(struct.unpack('<f', fheader[365:369])[0]*10)/10
+    # pingheader['StartGain'] = fheader[320]
+    # pingheader['SectorSize'] = floor(struct.unpack('<f', fheader[357:361])[0])
+    # pingheader['TrainAngle'] = floor(struct.unpack('<f', fheader[361:365])[0])
+    # pingheader['Absorption'] = floor(struct.unpack('<f', fheader[329:333])[0]*10)/10
+
+    # Get time
+    dstr = timeprefix + bin2ascii(fheader[14:27])
+    fmt = "%m%d%Y%H%M%S.%f"
+    time = datetime.datetime.strptime(dstr, fmt)
+
+    return time, pingheader
+
+
+def parse_switchCommand(scommand):
+    # Parse the switch commands unique to the 881A-GS Model
+    # Input:
+    #    scommand: Switch commands in binary format
+    # Output:
+    #    SwitchCommand: A dictionary containing information extracted
+
+    # Initialize the dictionary
+    SwitchCommand = {}
+
+    # Extract relevant data using the manual as a guide
+    SwitchCommand["Range"] = scommand[3]
+
+    # When converting a byte to binary in python, it doesn't automatically fill in the 8 bits
+    # SwitchCommand['Pause'] = bin(scommand[5])[2:].zfill(8)[0]
+    # SwitchCommand['Reverse'] = bin(scommand[5])[2:].zfill(8)[6]
+
+    # All conversions are found in the manual
+    SwitchCommand["StartGain"] = scommand[8]
+    SwitchCommand["LogF"] = 10 * (scommand[9] + 1)
+    SwitchCommand["Absorption"] = scommand[10] / 100
+    SwitchCommand["TrainAngle"] = 3 * scommand[11] - 180
+    SwitchCommand["SectorSize"] = 3 * scommand[12]
+    SwitchCommand["StepSize"] = 0.3 * scommand[13]
+    SwitchCommand["PulseLength"] = 10 * scommand[14]
+    SwitchCommand["MinRange"] = scommand[15] / 10
+    SwitchCommand["NDataPoints"] = scommand[19] * 10
+    SwitchCommand["DataBits"] = scommand[20]
+    SwitchCommand["Frequency"] = 175 + scommand[25] * 5
+
+    return SwitchCommand
+
+
+def parse_returnHeader(sheader, switches):
+    # Parse the return data header for the 881A-GS model sonar
+    # Inputs:
+    #    sheader: The return data header in binary form
+    #    switches: A dictionary of switch commands created using parseSwitchCommand()
+    # Output:
+    #    FanHeader: A dictionary containing data extracted from the return data header
+
+    # Initialize the dictionary
+    returnHeader = {}
+    # A 3-character string that used to determine the size of the sonar output
+    returnHeader["ReturnDataHeaderType"] = bin2ascii(sheader[0:3])
+    # ID of the transducer head
+    returnHeader["HeadID"] = hex(sheader[3])
+
+    # Extracted using Doug Wilson's method
+    returnHeader["HeadPosition"] = (63 & sheader[6]) * 128 + (127 & sheader[5])
+    returnHeader["HeadAngle"] = (returnHeader["HeadPosition"] - 600) * switches[
+        "StepSize"
+    ]
+
+    # Extract additional information
+    # Method for step direction taken from the manual
+    returnHeader["StepDirection"] = (sheader[6] & 64) >> 6
+    returnHeader["Range"] = sheader[7]
+
+    # The following variables are all extracted using methods provided by the manual
+    # Profile Range
+    HB = (sheader[9] & 0x7E) >> 1
+    LB = ((sheader[9] & 0x01) << 7) | (sheader[8] & 0x7F)
+    returnHeader["ProfileRange"] = (HB << 8) | LB
+
+    # Data Bytes
+    HB = (sheader[11] & 0x7E) >> 1
+    LB = ((sheader[11] & 0x01) << 7) | (sheader[10] & 0x7F)
+    returnHeader["NDataBytes"] = (HB << 8) | LB
+
+    # Sonar Position
+    HB = (sheader[13] & 0x7E) >> 1
+    LB = ((sheader[13] & 0x01) << 7) | (sheader[12] & 0x7F)
+    returnHeader["SonarPosition"] = (HB << 8) | LB
+    returnHeader["Angle"] = 0.3 * (returnHeader["SonarPosition"] - 600)
+
+    # Pitch
+    HB = (sheader[15] & 0x7E) >> 1
+    LB = ((sheader[15] & 0x01) << 7) | (sheader[14] & 0x7F)
+    returnHeader["Pitch"] = (
+        (((HB << 8) | LB) - 16384 * (int(bin(sheader[15])[-1]))) * 360 / 16384
+    )
+
+    # Roll
+    HB = (sheader[17] & 0x7E) >> 1
+    LB = ((sheader[17] & 0x01) << 7) | (sheader[16] & 0x7F)
+    returnHeader["Roll"] = ((HB << 8) | LB) * 360 / 16384
+
+    # Heading
+    HB = (sheader[19] & 0x7E) >> 1
+    LB = ((sheader[19] & 0x01) << 7) | (sheader[18] & 0x7F)
+    returnHeader["Heading"] = ((HB << 8) | LB) * 360 / 16384
+
+    # Gyro Heading
+    HB = (sheader[22] & 0x7E) >> 1
+    LB = ((sheader[22] & 0x01) << 7) | (sheader[21] & 0x7F)
+    returnHeader["GyroHeading"] = ((HB << 8) | LB) * 360 / 16384
+
+    # Use the previously extracted 3-character string and data from SwitchCommand to determine
+    # the amount of data points per ping. Table containing this information is in the manual
+    if returnHeader["ReturnDataHeaderType"] == "INA":
+        if switches["DataBits"] == 4:
+            returnHeader["NReturnBytes"] = 128
+            returnHeader["NPoints"] = 256
+        elif switches["DataBits"] == 8:
+            returnHeader["NReturnBytes"] = 252
+            returnHeader["NPoints"] = 252
+        elif switches["DataBits"] == 16:
+            returnHeader["NReturnBytes"] = 500
+            returnHeader["NPoints"] = 250
+    elif returnHeader["ReturnDataHeaderType"] == "INB":
+        if switches["DataBits"] == 4:
+            returnHeader["NReturnBytes"] = 252
+            returnHeader["NPoints"] = 504
+        elif switches["DataBits"] == 8:
+            returnHeader["NReturnBytes"] = 500
+            returnHeader["NPoints"] = 500
+        elif switches["DataBits"] == 16:
+            returnHeader["NReturnBytes"] = 500
+            returnHeader["NPoints"] = 250
+    elif returnHeader["ReturnDataHeaderType"] == "INC":
+        returnHeader["NPoints"] = 0
+    else:
+        print(
+            f"{returnHeader['ReturnDataHeaderType']} is not a recognized data header type"
+        )
+
+    return returnHeader
